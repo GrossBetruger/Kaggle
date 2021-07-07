4d98dfdb98e4bdd681614162e6edd87b47d2af78
Oren Koriat <oren@cynerio.co>
2021-07-07 12:28:00 +0300

    pre-processing inline comments

--- housing_prices/housing_price_oracle.py
+++ housing_prices/housing_price_oracle.py
@@ -1,6 +1,6 @@
-from typing import List
-
 import pandas as pd
+
+from typing import List
 from pathlib import Path
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error
@@ -14,28 +14,40 @@ def read_data(file_name):
 
 
 def pre_process_data(data: pd.DataFrame, oh_columns: List[str]):
-    le = LabelEncoder()
     columns = data.columns
     label_enc_columns = [c for c in columns if data[c].dtype
                    == 'object' and c not in oh_columns]
     numeric_columns = [c for c in columns if data[c].dtype
                    != 'object' and c not in oh_columns]
 
+    # separate into 3 frames (each requires different encoding)
     oh_data = data.drop(set(columns) - set(oh_columns), axis=1)
     numeric_data = data.drop(set(label_enc_columns) | set(oh_columns), axis=1)
     label_enc_data = data.drop(set(numeric_columns) | set(oh_columns), axis=1)
+
+    # Label Encode
+    le = LabelEncoder()
     label_enc_data = label_enc_data.apply(le.fit_transform)
+
+    # One-Hot Encode
     oh = OneHotEncoder(sparse=False, handle_unknown='ignore')
     oh.fit(oh_data[oh_columns])
     expanded_one_hot_columns = oh.get_feature_names(oh_columns)
     transformed_oh_data = pd.DataFrame(oh.transform(oh_data[oh_columns]), columns=expanded_one_hot_columns)
+
+    # Concat frames back together
     data = pd.concat([numeric_data, transformed_oh_data, label_enc_data], axis=1)
+
+    # Impute missing data
     imputer = SimpleImputer(strategy='mean')
-    columns = data.columns
+    columns = data.columns # imputer loses column names
     data = pd.DataFrame(imputer.fit_transform(data))
     data.columns = columns
+
+    # Separate target data
     y = data.SalePrice
     X = data[set(data.columns) - {"SalePrice"}]
+
     return X, y
 
 
