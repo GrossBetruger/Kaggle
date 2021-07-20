c06dfc3497804d2759b487cbc4574ef49973b288
Oren Koriat <oren@cynerio.co>
2021-07-21 01:59:53 +0300

    augment useful data

--- nlp/word_similarity.py
+++ nlp/word_similarity.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from numpy import ndarray
 from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
+import matplotlib.pyplot as plt
 
 RANDOM_STATE = 42
 
@@ -42,37 +43,41 @@ def word_similarity_main():
 
 def yelp_review_classifier_main():
     yelp_data = pd.read_csv(Path("data") / "yelp.csv")
-    print("data overview:")
+    print("yelp data overview:")
     print(yelp_data.describe())
 
     assert True not in yelp_data.stars.isnull().value_counts()  # no rows with missing target column
 
-    # since 1.409300 is the std for usefulness
-    # reviews with useful >= 3 are 2 stds to the right
-    # Todo: test 'useful' for normality
-    useful_reviews = yelp_data[yelp_data.useful >= 3]
-
-    # augment useful reviews
-    yelp_data = pd.concat([yelp_data])
-
-    # vectorize docs (average out words in every text to a single vector)
-    yelp_text = [nlp(text).vector for text in yelp_data.text.values]
-
     # create target label (reviews with more than 3 stars are good)
     y = pd.Series([1 if stars > 3 else 0 for stars in yelp_data.stars])
+    yelp_data["y"] = y # add target (used later for data augmentation)
 
     # split data
-    X_train, X_test, y_train, y_test = train_test_split(yelp_text,
+    X_train, X_test, y_train, y_test = train_test_split(yelp_data,
                                                         y,
                                                         test_size=0.1,
                                                         random_state=RANDOM_STATE)
+
+    # Augment useful tests for training set only (to avoid train-test contamination)
+    # since 1.409300 is the std for usefulness
+    # reviews with useful >= 3 are 2 stds to the right
+    # Todo: test 'useful' for normality
+    useful_reviews = X_train[X_train.useful >= 3]
+    useful_review_ys = useful_reviews.y
+    X_train = pd.concat([X_train, useful_reviews])
+    y_train = pd.concat([y_train, useful_review_ys])
+
+    # vectorize docs (average out words in every text to a single vector),
+    # remove all other features (including target: y)
+    X_train =  [nlp(text).vector for text in X_train.text.values]
+    X_test = [nlp(text).vector for text in X_test.text.values]
 
     # train and test SVC model
     svc = LinearSVC(random_state=RANDOM_STATE, max_iter=500)
     print("training...")
     svc.fit(X_train, y_train)
 
-    print(f"svc accuracy: {svc.score(X_test, y_test) * 100:.3f}%")
+    print(f"svc accuracy: {svc.score(X_test, y_test) * 100}")
 
 
 if __name__ == '__main__':
